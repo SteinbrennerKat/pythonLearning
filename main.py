6d20b8442c26f1221fae8ade8d336aeb38129423
Mark <mark.zilber@flolive.net>
2023-07-10 17:27:19 +0300

    Parse telegram channel message

--- main.py
+++
@@ -1,8 +0,0 @@
-from linkedin_api import Linkedin
-from os import environ
-
-api = Linkedin(environ.get('LINKEDIN_USR'), environ.get('LINKEDIN_PWD'))
-searchResult = api.search_people('Mark Zilber')
-print(searchResult)
-
-
